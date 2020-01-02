b5214a6a9bc03a622166d7800f4f571c23bd32f8
majida <majidaibenmansour@gmail.com>
2020-01-02 13:37:35 +0100

    update springadmin

--- springadmin/src/main/java/com/majida/springadmin/SpringadminApplication.java
+++ springadmin/src/main/java/com/majida/springadmin/SpringadminApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
 public class SpringadminApplication {
 
 	public static void main(String[] args) {
